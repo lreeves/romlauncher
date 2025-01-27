95ffe57b85961edff5bc59715b861a9dc9989358
Luke <luke@example.com>
2025-01-27 11:01:05 -0500

    Automatically run project

--- build.sh
+++ build.sh
@@ -16,3 +16,24 @@ podman build -t "${IMAGE_NAME}" .
 echo "Running container to build the project..."
 podman run --name "${CONTAINER_NAME}" --rm -v "${SOURCE_DIR}:/usr/src/project:Z" "${IMAGE_NAME}"
 
+if [ $? != 0 ]; then
+	echo "Build has failed"
+	exit $?
+fi
+
+LINE=`ps aux | grep "Ryujinx /home/codedemo/Source/romlauncher/project.nro" | grep -v grep`
+
+if [ $? == 0 ]; then
+	PROCESS_ID=`echo $LINE | cut -d ' ' -f 2`
+
+	echo "Killing existing Ryujinx process $PROCESS_ID"
+	kill $PROCESS_ID
+	sleep 3
+fi
+
+echo "Launching Ryujinx"
+
+DOTNET_ROOT=/home/codedemo/Tools/dotnet-sdk-8.0.405 /home/codedemo/Source/ryujinx/build/Ryujinx /home/codedemo/Source/romlauncher/project.nro >/home/codedemo/ryujinx.log
+
+cat /home/codedemo/.config/Ryujinx/sdcard/retrolauncher.log
+
