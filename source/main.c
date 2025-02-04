6d9fe79224fbe619d9a68cf71ef3f71ec53f9a6c
Luke <luke@example.com>
2025-02-04 11:59:00 -0500

    Don't pass anything rendering-related to list_files

--- source/main.c
+++ source/main.c
@@ -72,7 +72,7 @@ static int compare_strings(const void* a, const void* b) {
     return strcmp(*(const char**)a, *(const char**)b);
 }
 
-DirContent* list_files(const char* path, SDL_Renderer *renderer, TTF_Font *font, SDL_Color *colors, int selected_index) {
+DirContent* list_files(const char* path) {
     log_message("Starting to list files");
 
     DIR *dir;
@@ -283,7 +283,7 @@ int main(int argc, char** argv) {
     sound[3] = Mix_LoadWAV("data/pop4.wav");
 
     log_message("About to list files");
-    DirContent* content = list_files(rom_directory, renderer, font, colors, selected_index);
+    DirContent* content = list_files(rom_directory);
     if (content == NULL) {
         log_message("list_files returned NULL");
     } else {
