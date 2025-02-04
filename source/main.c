08d595e94fb1a1dde98baff3edceb9b923a59216
Luke <luke@example.com>
2025-02-04 11:16:54 -0500

    Remove copyright (it's a demo, nobody cares)

--- source/main.c
+++ source/main.c
@@ -1,24 +1,3 @@
-/* Mini SDL Demo
- * featuring SDL2 + SDL2_mixer + SDL2_image + SDL2_ttf
- * on Nintendo Switch using libnx
- *
- * Copyright 2018 carsten1ns
- *           2020 WinterMute
- *
- * Permission to use, copy, modify, and/or distribute this software for any
- * purpose with or without fee is hereby granted, provided that the above
- * copyright notice and this permission notice appear in all copies.
- *
- * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
- * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
- * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
- * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
- * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
- * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
- * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
- *
- */
-
 #include <time.h>
 #include <unistd.h>
 #include <stdio.h>
